3c6594a1807aabae0537e47dafc12b03b7c9c84c
Kandahar <said.imran@student.vinci.be>
2018-12-05 16:04:07 +0100

    webpack implementation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,13 +78,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lrxyguhi',
+        'USER': 'lrxyguhi',
+        'PASSWORD': 'VkpU069b9byp5eRqoayAJG6eGgxn7Fkp',
+        'HOST': 'elmer.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    modules: ["node_modules", "src/client"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
+  }
+};
